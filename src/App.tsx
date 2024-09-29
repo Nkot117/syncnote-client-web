48f3ca7439b5dcbf2b8d853fa7be220891a9a90e
Nkot117 <kumakura117@gmail.com>
2024-09-29 22:08:48 +0900

    ログインページのコンポーネントの枠組みを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
+import LoginLayout from "./components/layout/LoginLayout";
+import Login from "./components/pages/Login";
 
+const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginLayout />}>
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/layout/LoginLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const LoginLayout = () => {
+  return (
+    <>
+      <div>LoginLayout</div>
+      <Outlet />
+    </>
+  );
+};
+
+export default LoginLayout;
