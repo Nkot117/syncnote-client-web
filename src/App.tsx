97df2a3062bfc30cfeb7308262a6ff9c90352618
Nkot117 <kumakura117@gmail.com>
2024-09-29 22:20:53 +0900

    MUIを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createTheme } from "@mui/material/styles";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 import LoginLayout from "./components/layout/LoginLayout";
 import Login from "./components/pages/Login";
 
 const App = () => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#0000ff",
+      },
+    },
+  });
+
   return (
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginLayout />}>
@@ -12,6 +24,7 @@ const App = () => {
         </Route>
       </Routes>
     </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
