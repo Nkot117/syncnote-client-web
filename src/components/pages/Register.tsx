d79dec7e54f95bff1ebc25ad3b79b21c4b13ba62
Nkot117 <kumakura117@gmail.com>
2024-10-01 21:27:50 +0900

    共通で使うカードスタイルを作成

--- src/components/pages/Register.tsx
+++ src/components/pages/Register.tsx
@@ -4,31 +4,15 @@ import {
   FormControl,
   FormLabel,
   Link,
-  styled,
   TextField,
   Typography,
 } from "@mui/material";
-import MuiCard from "@mui/material/Card";
-
-const Card = styled(MuiCard)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignSelf: "center",
-  width: "100%",
-  padding: theme.spacing(4),
-  gap: theme.spacing(2),
-  margin: "auto",
-  [theme.breakpoints.up("sm")]: {
-    maxWidth: "450px",
-  },
-  boxShadow:
-    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
-}));
+import { LoginCard } from "../styles/LoginCardStyle";
 
 const Register = () => {
   return (
     <>
-      <Card>
+      <LoginCard>
         <Typography
           component="h1"
           variant="h4"
@@ -109,7 +93,7 @@ const Register = () => {
             </Link>
           </span>
         </Typography>
-      </Card>
+      </LoginCard>
     </>
   );
 };
